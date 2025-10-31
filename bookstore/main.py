a4f395da7c89695ade7655ed99259157e49e00d6
Arunava-27062003 <arunava.kundu@iemlabs>
2025-10-31 14:47:04 +0530

    Add endpoints for author retrieval and book listing with optional year parameter

--- bookstore/main.py
+++ bookstore/main.py
@@ -9,3 +9,21 @@ async def read_book(book_id: int):
         "title": "Sukumar Samagra",
         "author": "Sukumar Roy"
     }
+    
+# path parameter endpoint
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+    
+# query parameter at endpoint
+@app.get("/books")
+async def read_books(year: int = None): # here year is a optional parameter
+    if year:
+        return {
+            "year": year,
+            "book": ["Book 1", "Book 2"]
+        }
+    return { "books": ["All Books"] }
